9a02cc0a3aacdea1558adaf96e24d2498c8df2e7
dblant <dustinblntn@gmail.com>
2020-08-03 18:29:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -6,6 +6,17 @@ class App < Sinatra::Base
     erb :food_form
   end
 
-  # Add your post route and action below
+  # agent = "Carl Jr."
+  #     food = "Vegan Black-Bean Burger"
+
+  #     visit "/food_form"
+
+  #     fill_in("name", with: agent)
+  #     fill_in("favorite_food", with: food)
+
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
+  end
+  
 
 end
